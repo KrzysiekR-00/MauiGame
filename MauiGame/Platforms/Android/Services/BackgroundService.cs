32b512ba96caca17d2183dd63d8483cacad68d7d
KrzysiekR-00 <krzysztof.rygol@gmail.com>
2024-12-16 19:19:32 +0100

    Cleanup services

--- MauiGame/Platforms/Android/Services/BackgroundService.cs
+++ MauiGame/Platforms/Android/Services/BackgroundService.cs
@@ -8,37 +8,6 @@ namespace MauiGame.Platforms.Android.Services;
 [Service]
 public class BackgroundService : Service, IBackgroundService
 {
-    internal const int ServiceNotificationId = 1;
-
-    internal static NotificationChannel? ServiceNotificationChannel { get; private set; } = null;
-
-    public override StartCommandResult OnStartCommand(Intent? intent, StartCommandFlags flags, int startId)
-    {
-        ServiceNotificationChannel = new NotificationChannel(
-            "ServiceChannel",
-            "ServiceDemo",
-            NotificationImportance.Default);
-
-        var manager = (NotificationManager?)Platform.AppContext.GetSystemService(NotificationService);
-
-        if (manager != null)
-        {
-            manager.CreateNotificationChannel(ServiceNotificationChannel);
-
-            var notification = new Notification.Builder(this, ServiceNotificationChannel.Id)
-                .SetContentTitle("Service Working")
-                .SetContentText("Service Working")
-                .SetSmallIcon(Resource.Drawable.abc_ab_share_pack_mtrl_alpha)
-                .SetOngoing(true)
-                .SetContentIntent(GetPendingIntent())
-                .Build();
-
-            StartForeground(ServiceNotificationId, notification);
-        }
-
-        return StartCommandResult.NotSticky;
-    }
-
     public bool IsActive { get; private set; } = false;
 
     public void Start()
@@ -62,153 +31,21 @@ public class BackgroundService : Service, IBackgroundService
         throw new NotImplementedException();
     }
 
-    internal static PendingIntent? GetPendingIntent()
+    public override StartCommandResult OnStartCommand(Intent? intent, StartCommandFlags flags, int startId)
     {
-        var intent = new Intent(Platform.AppContext, typeof(MainActivity));
-        intent.AddFlags(ActivityFlags.NewTask);
-        intent.PutExtra("startMainPage", true);
-        return PendingIntent.GetActivity(
-            Platform.AppContext,
-            0,
-            intent,
-            PendingIntentFlags.UpdateCurrent);
-    }
-
-
-
-    /*
-
-    public bool IsActive { get; private set; } = false;
-
-    private NotificationManager _notificationManager;
-    private Notification _notification;
-
-    private int _notificationId = 100; // Unikalny identyfikator powiadomienia
-
-    private string _title = string.Empty;
-
-    public override IBinder OnBind(Intent intent)
-    {
-        throw new NotImplementedException();
-    }
-
-    [return: GeneratedEnum]
-    public override StartCommandResult OnStartCommand(Intent intent, [GeneratedEnum] StartCommandFlags flags, int startId)
-    {
-        if (intent.Action == "START_SERVICE")
+        if (Services.NotificationService.GetNotificationChannelId() != null)
         {
-            RegisterNotification();
+            var notification = Services.NotificationService.CreateNotification(
+                "Service working",
+                "Service working"
+                );
 
-            //MauiGame.Platforms.Android.Services.NotificationService.Instance.SendNotification("Title", "Message");
-        }
-        else if (intent.Action == "STOP_SERVICE")
-        {
-            StopForeground(true);
-            StopSelfResult(startId);
+            StartForeground(
+                Services.NotificationService.NotificationId,
+                notification
+                );
         }
 
         return StartCommandResult.NotSticky;
     }
-
-    public void Start()
-    {
-        //_title = title;
-
-        Intent startService = new Intent(MainActivity.ActivityCurrent, typeof(BackgroundService));
-        startService.SetAction("START_SERVICE");
-        MainActivity.ActivityCurrent.StartService(startService);
-
-        IsActive = true;
-    }
-
-    public void Stop()
-    {
-        Intent stopIntent = new Intent(MainActivity.ActivityCurrent, this.Class);
-        stopIntent.SetAction("STOP_SERVICE");
-        MainActivity.ActivityCurrent.StartService(stopIntent);
-
-        IsActive = false;
-    }
-
-    private void RegisterNotification()
-    {
-        // Tworzymy kanał powiadomień
-        //NotificationChannel channel = new NotificationChannel("ServiceChannel", "ServiceDemo", NotificationImportance.Default);
-        //_notificationManager = (NotificationManager)MainActivity.ActivityCurrent.GetSystemService(Context.NotificationService);
-        //_notificationManager.CreateNotificationChannel(channel);
-
-        //// Tworzymy powiadomienie
-        //_notification = new Notification.Builder(this, "ServiceChannel")
-        //    .SetContentTitle("Service Working")
-        //    //.SetContentText("Service is still running")
-        //    .SetSmallIcon(Resource.Drawable.abc_ab_share_pack_mtrl_alpha)
-        //    .SetOngoing(true)
-        //    .SetContentIntent(GetPendingIntent()) // Kliknięcie powiadomienia uruchamia aplikację
-        //    .Build();
-
-        //// Uruchamiamy powiadomienie w tle
-
-        var notification = MauiGame.Platforms.Android.Services.NotificationService.Instance.GetNotification("Service Working", "Service Working", this);
-
-        var id = MauiGame.Platforms.Android.Services.NotificationService.Instance.MessageId;
-
-        StartForeground(id, notification);
-    }
-
-    /*
-    // Metoda do zmiany tytułu istniejącego powiadomienia
-    public void SetTitle(string title)
-    {
-        _title = title;
-
-        if (_notificationManager == null)
-        {
-            _notificationManager = (NotificationManager)MainActivity.ActivityCurrent.GetSystemService(Context.NotificationService);
-        }
-
-        // Tworzymy nowe powiadomienie z nowym tytułem
-        _notification = new Notification.Builder(MainActivity.ActivityCurrent, "ServiceChannel")
-            .SetContentTitle(title)
-            //.SetContentText("Service is still running")
-            .SetSmallIcon(Resource.Drawable.abc_ab_share_pack_mtrl_alpha)
-            .SetOngoing(true)
-            .SetContentIntent(GetPendingIntent()) // Kliknięcie powiadomienia uruchamia aplikację
-            .Build();
-
-        // Zaktualizuj istniejące powiadomienie
-        _notificationManager.Notify(_notificationId, _notification);
-    }
-
-    private void RegisterNotification()
-    {
-        // Tworzymy kanał powiadomień
-        NotificationChannel channel = new NotificationChannel("ServiceChannel", "ServiceDemo", NotificationImportance.Default);
-        _notificationManager = (NotificationManager)MainActivity.ActivityCurrent.GetSystemService(Context.NotificationService);
-        _notificationManager.CreateNotificationChannel(channel);
-
-        // Tworzymy powiadomienie
-        _notification = new Notification.Builder(this, "ServiceChannel")
-            .SetContentTitle("Service Working")
-            //.SetContentText("Service is still running")
-            .SetSmallIcon(Resource.Drawable.abc_ab_share_pack_mtrl_alpha)
-            .SetOngoing(true)
-            .SetContentIntent(GetPendingIntent()) // Kliknięcie powiadomienia uruchamia aplikację
-            .Build();
-
-        // Uruchamiamy powiadomienie w tle
-        StartForeground(_notificationId, _notification);
-    }
-    
-    // Tworzymy PendingIntent, który uruchomi aplikację po kliknięciu powiadomienia
-    private PendingIntent GetPendingIntent()
-    {
-        // Stwórz Intent, który uruchomi główną stronę aplikacji MAUI (MainPage)
-        var intent = new Intent(this, typeof(MainActivity));
-        intent.AddFlags(ActivityFlags.NewTask); // Umożliwia uruchomienie nowej aktywności w nowym zadaniu
-
-        // Otwórz stronę aplikacji MAUI, nie tylko aktywność
-        intent.PutExtra("startMainPage", true); // Możesz dodać dane do Intent, jeśli chcesz, aby MainActivity wykonała specjalne akcje
-        return PendingIntent.GetActivity(MainActivity.ActivityCurrent, 0, intent, PendingIntentFlags.UpdateCurrent);
-    }
-    */
 }
--- MauiGame/Platforms/Android/Services/NotificationService.cs
+++ MauiGame/Platforms/Android/Services/NotificationService.cs
@@ -1,4 +1,5 @@
 ﻿using Android.App;
+using Android.Content;
 using AndroidX.Core.App;
 using MauiGame.Services;
 
@@ -6,175 +7,61 @@ namespace MauiGame.Platforms.Android.Services;
 
 public class NotificationService : INotificationService
 {
+    internal const int NotificationId = 1;
+
+    private static NotificationChannel? _notificationChannel = null;
+
     public void Show(string title, string message)
     {
-        if (BackgroundService.ServiceNotificationChannel == null) return;
+        if (GetNotificationChannelId() == null) return;
 
         var compatManager = NotificationManagerCompat.From(Platform.AppContext);
 
-        var notification = new Notification.Builder(Platform.AppContext, BackgroundService.ServiceNotificationChannel.Id)
+        var notification = CreateNotification(title, message);
+
+        compatManager.Notify(NotificationId, notification);
+    }
+
+    internal static Notification CreateNotification(string title, string message)
+    {
+        return new Notification.Builder(Platform.AppContext, GetNotificationChannelId())
                 .SetContentTitle(title)
                 .SetContentText(message)
                 .SetSmallIcon(Resource.Drawable.abc_ab_share_pack_mtrl_alpha)
                 .SetOngoing(true)
-                .SetContentIntent(BackgroundService.GetPendingIntent())
+                .SetContentIntent(GetPendingIntent())
                 .Build();
-
-        compatManager.Notify(BackgroundService.ServiceNotificationId, notification);
     }
 
-    /*
-    const string channelId = "default";
-    const string channelName = "Default";
-    const string channelDescription = "The default channel for notifications.";
-
-    public const string TitleKey = "title";
-    public const string MessageKey = "message";
-
-    bool channelInitialized = false;
-    internal int MessageId { get; } = 0;
-    int pendingIntentId = 0;
-
-    NotificationManagerCompat compatManager;
-
-    public event EventHandler NotificationReceived;
-
-    public static NotificationService Instance { get; private set; }
-
-    public NotificationService()
+    internal static string? GetNotificationChannelId()
     {
-        if (Instance == null)
+        if (_notificationChannel == null)
         {
-            CreateNotificationChannel();
-            compatManager = NotificationManagerCompat.From(Platform.AppContext);
-            Instance = this;
-        }
-    }
+            _notificationChannel = new NotificationChannel(
+                "ServiceChannel",
+                "ServiceDemo",
+                NotificationImportance.Default);
 
-    public void SendNotification(string title, string message, DateTime? notifyTime = null)
-    {
-        if (!channelInitialized)
-        {
-            CreateNotificationChannel();
-        }
+            var manager = (NotificationManager?)Platform.AppContext.GetSystemService(BackgroundService.NotificationService);
 
-        if (notifyTime != null)
-        {
-            //Intent intent = new Intent(Platform.AppContext, typeof(AlarmHandler));
-            //intent.PutExtra(TitleKey, title);
-            //intent.PutExtra(MessageKey, message);
-            //intent.SetFlags(ActivityFlags.SingleTop | ActivityFlags.ClearTop);
-
-            //var pendingIntentFlags = (Build.VERSION.SdkInt >= BuildVersionCodes.S)
-            //    ? PendingIntentFlags.CancelCurrent | PendingIntentFlags.Immutable
-            //    : PendingIntentFlags.CancelCurrent;
-
-            //PendingIntent pendingIntent = PendingIntent.GetBroadcast(Platform.AppContext, pendingIntentId++, intent, pendingIntentFlags);
-            //long triggerTime = GetNotifyTime(notifyTime.Value);
-            //AlarmManager alarmManager = Platform.AppContext.GetSystemService(Context.AlarmService) as AlarmManager;
-            //alarmManager.Set(AlarmType.RtcWakeup, triggerTime, pendingIntent);
-        }
-        else
-        {
-            Show(title, message);
-        }
-    }
-
-    public void ReceiveNotification(string title, string message)
-    {
-        //var args = new NotificationEventArgs()
-        //{
-        //    Title = title,
-        //    Message = message,
-        //};
-        //NotificationReceived?.Invoke(null, args);
-    }
-
-    public void Show(string title, string message)
-    {
-        //Intent intent = new Intent(Platform.AppContext, typeof(MainActivity));
-        //intent.PutExtra(TitleKey, title);
-        //intent.PutExtra(MessageKey, message);
-        //intent.SetFlags(ActivityFlags.SingleTop | ActivityFlags.ClearTop);
-
-        //var pendingIntentFlags = (Build.VERSION.SdkInt >= BuildVersionCodes.S)
-        //    ? PendingIntentFlags.UpdateCurrent | PendingIntentFlags.Immutable
-        //    : PendingIntentFlags.UpdateCurrent;
-
-        //PendingIntent pendingIntent = PendingIntent.GetActivity(Platform.AppContext, pendingIntentId++, intent, pendingIntentFlags);
-
-        //NotificationCompat.Builder builder = new NotificationCompat.Builder(Platform.AppContext, channelId)
-        //    .SetContentIntent(pendingIntent)
-        //    .SetContentTitle(title)
-        //    .SetContentText(message)
-        //    .SetOngoing(true)
-        //    //.SetLargeIcon(BitmapFactory.DecodeResource(Platform.AppContext.Resources, Resource.Drawable.dotnet_logo))
-        //    //.SetSmallIcon(Resource.Drawable.message_small);
-        //    .SetSmallIcon(Resource.Drawable.abc_ab_share_pack_mtrl_alpha);
-
-        //Notification notification = builder.Build();
-        //compatManager.Notify(messageId++, notification);
-
-        var notification = GetNotification(title, message, Platform.AppContext);
-
-        compatManager.Notify(MessageId, notification);
-    }
-
-    internal Notification GetNotification(string title, string message, Context context)
-    {
-        if (!channelInitialized)
-        {
-            CreateNotificationChannel();
-        }
-
-        Intent intent = new Intent(Platform.AppContext, typeof(MainActivity));
-        intent.PutExtra(TitleKey, title);
-        intent.PutExtra(MessageKey, message);
-        intent.SetFlags(ActivityFlags.SingleTop | ActivityFlags.ClearTop);
-
-        var pendingIntentFlags = (Build.VERSION.SdkInt >= BuildVersionCodes.S)
-            ? PendingIntentFlags.UpdateCurrent | PendingIntentFlags.Immutable
-            : PendingIntentFlags.UpdateCurrent;
-
-        PendingIntent pendingIntent = PendingIntent.GetActivity(Platform.AppContext, pendingIntentId++, intent, pendingIntentFlags);
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, channelId)
-            .SetContentIntent(pendingIntent)
-            .SetContentTitle(title)
-            .SetContentText(message)
-            .SetOngoing(true)
-            //.SetLargeIcon(BitmapFactory.DecodeResource(Platform.AppContext.Resources, Resource.Drawable.dotnet_logo))
-            //.SetSmallIcon(Resource.Drawable.message_small);
-            .SetSmallIcon(Resource.Drawable.abc_ab_share_pack_mtrl_alpha);
-
-        Notification notification = builder.Build();
-
-        return notification;
-    }
-
-    private void CreateNotificationChannel()
-    {
-        // Create the notification channel, but only on API 26+.
-        if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
-        {
-            var channelNameJava = new Java.Lang.String(channelName);
-            var channel = new NotificationChannel(channelId, channelNameJava, NotificationImportance.Default)
+            if (manager != null)
             {
-                Description = channelDescription
-            };
-            // Register the channel
-            NotificationManager manager = (NotificationManager)Platform.AppContext.GetSystemService(Context.NotificationService);
-            manager.CreateNotificationChannel(channel);
-            channelInitialized = true;
+                manager.CreateNotificationChannel(_notificationChannel);
+            }
         }
+
+        return _notificationChannel.Id;
     }
 
-    private long GetNotifyTime(DateTime notifyTime)
+    private static PendingIntent? GetPendingIntent()
     {
-        DateTime utcTime = TimeZoneInfo.ConvertTimeToUtc(notifyTime);
-        double epochDiff = (new DateTime(1970, 1, 1) - DateTime.MinValue).TotalSeconds;
-        long utcAlarmTime = utcTime.AddSeconds(-epochDiff).Ticks / 10000;
-        return utcAlarmTime; // milliseconds
+        var intent = new Intent(Platform.AppContext, typeof(MainActivity));
+        intent.AddFlags(ActivityFlags.NewTask);
+        intent.PutExtra("startMainPage", true);
+        return PendingIntent.GetActivity(
+            Platform.AppContext,
+            0,
+            intent,
+            PendingIntentFlags.UpdateCurrent);
     }
-    */
 }
