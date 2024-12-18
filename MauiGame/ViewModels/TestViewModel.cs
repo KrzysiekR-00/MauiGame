10058317ab84968f3a0f10859d4d485ea53cec79
KrzysiekR-00 <krzysztof.rygol@gmail.com>
2024-12-18 19:36:43 +0100

    Navigate back to main view from test view

--- MauiGame/ViewModels/TestViewModel.cs
+++ MauiGame/ViewModels/TestViewModel.cs
@@ -1,8 +1,23 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MauiGame.Services;
 
 namespace MauiGame.ViewModels;
 public partial class TestViewModel : ObservableObject
 {
+    private readonly INavigationService _navigationService;
+
     [ObservableProperty]
     private string _test = "test";
+
+    public TestViewModel()
+    {
+        _navigationService = new NavigationService();
+    }
+
+    [RelayCommand]
+    private void NavigationTest()
+    {
+        _navigationService.NavigateToAsync("//MainView");
+    }
 }
